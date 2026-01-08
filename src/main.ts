49f7cacc78cf640aa01abec43a11228abe4cff0f
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2026-01-08 20:32:50 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,7 @@ async function bootstrap() {
 
   // Enable CORS for frontend
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'http://localhost:3000',
-      'https://seu-app.vercel.app',
-    ],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
